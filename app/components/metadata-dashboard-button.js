dc1d9291e7c5ff2e3595f6b5c8d0457c96491225
Suzanne Vogt <suzanne.vogt@datacite.org>
2026-03-13 19:13:39 -0400

    Add username to the metadata dashboard url.

--- app/components/metadata-dashboard-button.js
+++ app/components/metadata-dashboard-button.js
@@ -1,9 +1,12 @@
 import Component from '@glimmer/component';
 import ENV from 'bracco/config/environment';
+import { inject as service } from '@ember/service';
 
 export default class MetadataDashboardButton extends Component {
+  @service currentUser;
+
   get metadataDashboardUrl() {
-    return ENV.METADATA_DASHBOARD_URL;
+    return ENV.METADATA_DASHBOARD_URL + "/" + this.currentUser.uid;
   }
   get fabricaDeployTarget() {
     return ENV.fabricaDeployTarget;
